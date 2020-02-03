54f27247dee677288556cc03fce825e8171f178c
Simone <simonelega.sl@gmail.com>
2020-02-03 15:34:42 +0100

    Generato dinamicamente nome mese

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-var month = moment("2018-02", "YYYY-MM").daysInMonth() // 29
-console.log(month);
 
+
+
+
+var monthNames = [
+  'gennaio',
+  'febbraio',
+  'marzo',
+  'aprile',
+  'maggio',
+  'giugno',
+  'luglio',
+  'agosto',
+  'settembre',
+  'ottobre',
+  'novembre',
+  'dicembre'
+];
+var n = 1;
+var monthName = monthNames[n-1];
+$('.month_name').text('mese di ' + monthName);
+
+var month = moment("2018-0"+n+"", "YYYY-MM").daysInMonth(); // 29
+console.log("2018-0"+n+"");
+console.log(month);
 for (var i = 1; i <= month; i++) {
   var source = document.getElementById("entry-template").innerHTML;
   var template = Handlebars.compile(source);
