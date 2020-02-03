6ca11f419200df7fdaa3e1fc86791f9c7252387b
Simone <simonelega.sl@gmail.com>
2020-02-03 15:11:18 +0100

    Calendario generato dinamicamente con utilizzo di Handlebars e moment()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert('ciao');
+var month = moment("2018-02", "YYYY-MM").daysInMonth() // 29
+console.log(month);
+
+for (var i = 1; i <= month; i++) {
+  var source = document.getElementById("entry-template").innerHTML;
+  var template = Handlebars.compile(source);
+  var context = { numero: i };
+  var html = template(context);
+  $('.calendar').append(html);
+};
