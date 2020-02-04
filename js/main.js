8338e3708b36a40fa3167df0d89ff7aa5af1eb27
Simone <simonelega.sl@gmail.com>
2020-02-04 18:49:40 +0100

    Modifiche come da lezione

--- js/main.js
+++ js/main.js
@@ -1,107 +1,108 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  // var monthNames = [
-  //   'gennaio',
-  //   'febbraio',
-  //   'marzo',
-  //   'aprile',
-  //   'maggio',
-  //   'giugno',
-  //   'luglio',
-  //   'agosto',
-  //   'settembre',
-  //   'ottobre',
-  //   'novembre',
-  //   'dicembre'
-  // ];
 
-  var monthName="Gennaio";
-
-  var n = 1;
-  cambioMese()
-  $('.next').click(function(){
-    $('.calendar ul li').remove();
-    n++;
-    if(n > 12){
-      n = 1;
+  var thisMonth = 0;
+  var year = 2018;
+  var baseMonth = moment(
+    {
+      year: year,
+      month: thisMonth
     }
-    cambioMese();
-  });
-  $('.prev').click(function(){
-    $('.calendar ul li').remove();
-    n--;
-    if(n < 1){
-      n = 12;
-    }
-    cambioMese();
-  });
-  function cambioMese(){
-    var monthName = monthNames[n-1];
-    $('.month_name').text('mese di ' + monthName);
-
-    var month = moment("2018-0"+n+"", "YYYY-MM").daysInMonth();
-    for (var i = 1; i <= month; i++) {
-      var source = document.getElementById("entry-template").innerHTML;
-      var template = Handlebars.compile(source);
-      var context = { numero : i, mese : monthNames[n - 1] };
-      var html = template(context);
-      $('.calendar ul').append(html);
-    };
-  };
-
-
-  $.ajax(
-  {
-  url: "https://flynn.boolean.careers/exercises/api/holidays",
-  method: "GET",
-  success: function (data, stato) {
-    var dataChange =[];
-    var dataHoliday = [];
-   for (var i = 0; i < data.response.length; i++) {
-     dataChange.push(moment(data.response[i].date));
-     dataHoliday.push(dataChange[i].format('d'));
-   };
-
-    calendarLi = $('.calendar ul li');
-     // for (var j = 0; j < calendarLi.length; j++) {
-     //    if(calendarLi[j].hasClass(3)){
-     //      console.log(calendarLi[j]);
-     //    }
-     // }
-    for (var j = 0; j < calendarLi.length; j++) {
-      var numero = $('.day'+j+'').text();
-      dataHoliday.toString();
-      console.log(""+dataHoliday[j]+"");
-    //   if($('.day'+j+''':contains(dataHoliday['+j+']')){
-    //     console.log(j);
-    //   };
-    //   console.log($('.day'+j+''));
-    // }
-    // console.log(dataHoliday[0]);
-    // $('calendar ul li').addClass('red');
-    //   if($('.calendar ul li').hasClass(''+dataHoliday[0]+'')){
-    //     calendarLi[6].addClass('red');
-    //     console.log('sì in posizione '+j);
-    //   }
-    }
-  },
-  error: function (richiesta, stato, errori) {
-  alert("E' avvenuto un errore. " + errore);
-  }
-  }
   );
 
+
+
+
+  printMonth(baseMonth);
+  printHoliday(baseMonth);
+
+
+  $('#next').click(function () {
+
+    var thisMonth = $('h1').attr('data-this-month');
+    var date = moment(thisMonth).add(1, 'months');
+    console.log(date);
+
+
+    printMonth(date);
+    printHoliday(date);
+  });
+
+  $('#prev').click(function () {
+    var thisMonth = $('h1').attr('data-this-month');
+    var date = moment(thisMonth).subtract(1, 'months');
+    console.log(date);
+
+
+    printMonth(date);
+    printHoliday(date);
+  });
+
 });
-// {
-//     "success": true,
-//     "response": [
-//         {
-//             "name": "Capodanno",
-//             "date": "2018-01-01"
-//         },
-//         {
-//             "name": "Epifania",
-//             "date": "2018-01-06"
-//         }
-//     ]
-// }
+
+
+
+// FUNCTION -----------------------
+
+function printMonth(month) {
+  $('.days').html('');
+
+  $('h1').text(month.format('MMMM YYYY'));
+  $('h1').attr('data-this-month', month.format('YYYY-MM'));
+
+
+  var daysInMonth = month.daysInMonth();
+
+
+  for (var i = 1; i <= daysInMonth ; i++) {
+
+
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+    var context = {
+      day: i,
+      month: month.format('MMMM'),
+      dateComplete: month.format('YYYY-MM') + '-' + addZero(i)
+    };
+    var html = template(context);
+    $('.days').append(html);
+  }
+}
+
+function addZero(num) {
+  if(num < 10) {
+    return '0' + num;
+  }
+  return num;
+}
+
+
+function printHoliday(month) {
+  $.ajax(
+    {
+      url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+      method: 'GET',
+      data: {
+        year: month.year(),
+        month: month.month()
+      },
+      success: function (data) {
+
+        var holidays = data.response;
+
+        for (var i = 0; i < holidays.length; i++) {
+          var thisHoliday = holidays[i];
+
+          var thisHolidayData = thisHoliday.date;
+
+          $('li[data-date-complete="'+ thisHolidayData  +'"]').addClass('red');
+          $('li[data-date-complete="'+ thisHolidayData  +'"]').find('.nome-festivita').append(thisHoliday.name);
+          // 'li[data-date-complete="2018-01-01"]'
+        }
+      },
+      error: function () {
+        alert('errore');
+      }
+    }
+  );
+}
